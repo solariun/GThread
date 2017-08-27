255399462acb0b35edd47eec94c61b6ad82d7c4c
Gustavo Campos <lgustavocampos@gmail.com>
2017-08-27 01:25:12 -0300

    Initial Commit

---
+++ GThread/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GThread
+//
+//  Created by GUSTAVO CAMPOS on 27/08/17.
+//  Copyright © 2017 GUSTAVO CAMPOS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
